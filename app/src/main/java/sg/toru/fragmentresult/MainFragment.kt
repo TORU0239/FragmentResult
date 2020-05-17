c58be559ea5c886ad63083774b7034b72a9c5a61
Toru Choi <blueocean239@gmail.com>
2020-05-17 22:34:35 +0800

    add simple code

--- app/src/main/java/sg/toru/fragmentresult/MainFragment.kt
+++ app/src/main/java/sg/toru/fragmentresult/MainFragment.kt
@@ -5,16 +5,16 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setFragmentResultListener("requestKey"){ resultKey, bundle ->
-            Toast.makeText(context, "result Key: $resultKey", Toast.LENGTH_SHORT).show()
-        }
     }
 
     private lateinit var edNameField:EditText
@@ -25,7 +25,13 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         edNameField = view.findViewById(R.id.edNameField)
         btnSendData = view.findViewById(R.id.btnSendDataFromA)
         btnSendData.setOnClickListener {
+            if(edNameField.editableText.isEmpty()) return@setOnClickListener
 
+            setFragmentResult("requestKey", bundleOf("name" to edNameField.editableText.toString()))
+            parentFragmentManager.commit {
+                replace(R.id.container, SecondFragment::class.java, null)
+                addToBackStack(null)
+            }
         }
     }
 
--- app/src/main/java/sg/toru/fragmentresult/SecondFragment.kt
+++ app/src/main/java/sg/toru/fragmentresult/SecondFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.setFragmentResultListener
 
-class SecondFragment : Fragment(R.layout.fragment_main) {
+class SecondFragment : Fragment(R.layout.fragment_second) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setFragmentResultListener("requestKey"){ _, bundle ->
+            Toast.makeText(context, "content: ${bundle.getString("name")}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
