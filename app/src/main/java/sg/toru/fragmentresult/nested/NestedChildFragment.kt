bb3106b1f567c300e32207a1c45c5bb878a8dd3a
Toru Choi <blueocean239@gmail.com>
2020-05-18 01:25:37 +0800

    add fragmentResult

--- app/src/main/java/sg/toru/fragmentresult/nested/NestedChildFragment.kt
+++ app/src/main/java/sg/toru/fragmentresult/nested/NestedChildFragment.kt
@@ -1,12 +1,32 @@
 package sg.toru.fragmentresult.nested
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import sg.toru.fragmentresult.R
 
 
 class NestedChildFragment : Fragment(R.layout.fragment_nested_child) {
+
+    private lateinit var editText: EditText
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        editText = view.findViewById(R.id.ed_nested_typing)
+        button = view.findViewById(R.id.btn_nested_send)
+
+        button.setOnClickListener {
+            if(editText.editableText.isEmpty()) return@setOnClickListener
+            setFragmentResult("NESTED_REQUEST_KEY", bundleOf("typed" to editText.editableText.toString()))
+        }
     }
 }
--- app/src/main/java/sg/toru/fragmentresult/nested/NestedParentFragment.kt
+++ app/src/main/java/sg/toru/fragmentresult/nested/NestedParentFragment.kt
@@ -1,12 +1,31 @@
 package sg.toru.fragmentresult.nested
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentResultListener
 import sg.toru.fragmentresult.R
 
 
 class NestedParentFragment : Fragment(R.layout.fragment_nested_parent) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        childFragmentManager.setFragmentResultListener(
+            "NESTED_REQUEST_KEY",
+            this,
+            FragmentResultListener { _, result ->
+                textFromChild.text = result.getString("typed")
+            }
+        )
+    }
+
+    private lateinit var textFromChild:TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textFromChild = view.findViewById(R.id.txt_from_child)
     }
 }
