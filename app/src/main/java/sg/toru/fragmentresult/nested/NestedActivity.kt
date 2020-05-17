f70a4246f1dccaf3ee6833899169e36989c8baad
Toru Choi <blueocean239@gmail.com>
2020-05-18 00:21:40 +0800

    create new activity to have nested fragment

---
+++ app/src/main/java/sg/toru/fragmentresult/nested/NestedActivity.kt
@@ -0,0 +1,11 @@
+package sg.toru.fragmentresult.nested
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sg.toru.fragmentresult.R
+
+class NestedActivity : AppCompatActivity(R.layout.activity_nested) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
