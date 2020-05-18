8085ab7363d54b8fe4136b5c9ae59ed7e82f6e9f
Toru Choi <blueocean239@gmail.com>
2020-05-18 14:58:33 +0800

    showing request key as well

--- app/src/main/java/sg/toru/fragmentresult/send/SendResultFragment.kt
+++ app/src/main/java/sg/toru/fragmentresult/send/SendResultFragment.kt
@@ -10,8 +10,8 @@ class SendResultFragment : Fragment(R.layout.fragment_second) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setFragmentResultListener("requestKey"){ _, bundle ->
-            Toast.makeText(context, "content: ${bundle.getString("name")}", Toast.LENGTH_SHORT).show()
+        setFragmentResultListener("requestKey"){ resultKey, bundle ->
+            Toast.makeText(context, "resultKey: $resultKey, content: ${bundle.getString("name")}", Toast.LENGTH_SHORT).show()
         }
     }
 
